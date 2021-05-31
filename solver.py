f99b00570c07d6267d5c9c78dbe9920b7257754c
Justin Yan <yan.justin1998@gmail.com>
2021-05-30 19:28:59 -0500

    fix incremental prune

--- solver.py
+++ solver.py
@@ -12,19 +12,20 @@ class WordSet:
         self._word_length = length
         self._pruned_letters = set()
 
-    def incremental_wordset_prune(self, char: str, idx: int) -> None:
-        if idx >= self._word_length:
-            raise ValueError(f'Invalid index: {idx}. Maximum index: {self._word_length - 1}')
-
-        if len(char) != 1:
+    def incremental_prune(self, letter: str) -> None:
+        if len(letter) != 1:
             raise ValueError(f'Can only prune by letter.')
 
-        if char in self._pruned_letters:
-            raise ValueError(f'Letter {char} already guessed.')
+        if letter in self._pruned_letters:
+            raise ValueError(f'Letter {letter} already guessed.')
 
+        removed = set()
         for word in self._wordset:
-            if word[idx] != char:
-                self._wordset.remove(word)
+            if letter in word:
+                removed.add(word)
+
+        self._wordset = self._wordset.difference(removed)
+        self._pruned_letters.add(letter)
         return
 
 
