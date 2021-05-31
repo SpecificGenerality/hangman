04dd7fd28865005ae2e81b7b6a9eb20731dce9e1
Justin Yan <yan.justin1998@gmail.com>
2021-05-31 11:53:23 -0500

    debug

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def str_to_np_arr(s: str):
   return np.array([ord(char) for char in s])
 
@@ -10,13 +11,13 @@ class Hangman():
     words: str,
   ):
     self.words = str_to_np_arr(words)
-    self.filled = np.zeros(a.shape, dtype=bool)
+    self.filled = np.zeros(self.words.shape, dtype=bool)
     self.attempts = 0
     self.misses = 0
   def guess(self, char: int):
     self.attempts += 1
-    idxs, = np.nonzero(self.words - char)
-    if not np.any(idxs): self.misses += 1
+    idxs = np.where((self.words - char) == 0)[0]
+    if len(idxs) == 0: self.misses += 1
     self.filled[idxs] = True
     return idxs
 
--- generator.py
+++ generator.py
@@ -1,33 +1,34 @@
+import csv
+import os
 from typing import List
 
 import numpy as np
-from numpy.core.fromnumeric import sort
 
 from constants import MAX_DIFFICULTY, MIN_DIFFICULTY
 
-import csv
-import os
-
 
 class Generator:
-    @classmethod
-    def generate_word_by_frequency(cls, words: List[str], frequencies: np.array, difficulty: int) -> str:
-  if difficulty < MIN_DIFFICULTY or difficulty > MAX_DIFFICULTY:
-    raise ValueError(f'Difficulty must be between {MIN_DIFFICULTY} and {MAX_DIFFICULTY}, inclusive.')
+  @classmethod
+  def generate_word_by_frequency(cls, words: List[str], frequencies: np.array, difficulty: int) -> str:
+    if difficulty < MIN_DIFFICULTY or difficulty > MAX_DIFFICULTY:
+      raise ValueError(f'Difficulty must be between {MIN_DIFFICULTY} and {MAX_DIFFICULTY}, inclusive.')
 
-  if len(words) != len(frequencies):
-    raise ValueError('Mismatch in length of words and frequencies lists.')
+    if len(words) != len(frequencies):
+      raise ValueError('Mismatch in length of words and frequencies lists.')
 
-  normalized_frequencies = frequencies / max(frequencies)
-  sorted_idxs = np.argsort(normalized_frequencies)
-  normalized_frequencies = normalized_frequencies[sorted_idxs]
-  sorted_words = np.array(words)[sorted_idxs]
-  bootstrap_cdf = np.cumsum(normalized_frequencies / sum(normalized_frequencies))
-  lower_bound = 0 if difficulty == 1 else (difficulty - 1) / MAX_DIFFICULTY
-  upper_bound = difficulty / MAX_DIFFICULTY
-  lower_idx = np.where(bootstrap_cdf >= lower_bound)[0][0]
-  upper_idx = np.where(bootstrap_cdf >= upper_bound)[0][0] if difficulty < MAX_DIFFICULTY else len(words)
-  return np.random.choice(sorted_words[lower_idx:upper_idx])
+    if len(words) < MAX_DIFFICULTY - MIN_DIFFICULTY:
+      raise ValueError('Not enough words.')
+
+    normalized_frequencies = frequencies / max(frequencies)
+    sorted_idxs = np.argsort(normalized_frequencies)
+    normalized_frequencies = normalized_frequencies[sorted_idxs]
+    sorted_words = np.array(words)[sorted_idxs]
+    bootstrap_cdf = np.cumsum(normalized_frequencies / sum(normalized_frequencies))
+    lower_bound = 0 if difficulty == 1 else (difficulty - 1) / MAX_DIFFICULTY
+    upper_bound = difficulty / MAX_DIFFICULTY
+    lower_idx = np.where(bootstrap_cdf >= lower_bound)[0][0]
+    upper_idx = np.where(bootstrap_cdf >= upper_bound)[0][0] if difficulty < MAX_DIFFICULTY else len(words)
+    return np.random.choice(sorted_words[lower_idx:upper_idx])
 
 def load_generator_csv():
   words = []
